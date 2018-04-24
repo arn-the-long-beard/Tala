4cc83ce295a0f36fe938192448d885afc78e1cab
ArnTheLongBeard <arn.menant@gmail.com>
2018-04-24 22:48:59 +0200

    add register

--- src/actions/auth.cr
+++ src/actions/auth.cr
@@ -3,7 +3,7 @@ require "json"
 require "jwt"
 
 def auth(credentials : String)
-    response =HTTP::Client.post("http://localhost:8529/_db/Tala/Tala/auth/login", headers: HTTP::Headers{"User-Agent" => "AwesomeApp"}, body: credentials)
+    response =HTTP::Client.post(DB_URL+"/auth/login", headers: HTTP::Headers{"User-Agent" => "AwesomeApp"}, body: credentials)
       # => 200
     code = response.status_code # => "<!doctype html>" 
     res = JSON.parse response.body
@@ -20,6 +20,22 @@ def auth(credentials : String)
     end
 end
 
+def register(credentials : String)
+    response =HTTP::Client.post(DB_URL+"auth/signup", headers: HTTP::Headers{"User-Agent" => "AwesomeApp"}, body: credentials)
+      # => 200
+    code = response.status_code # => "<!doctype html>" 
+    res = JSON.parse response.body
+        puts  response.status_code
+        puts res
+    if code  = 200
+        payload = { "sid" => response.cookies["sid"].value }
+        token = JWT.encode(payload, SECRET, "HS256")
+    #tokenjson = {"token" => token }
 
+    return {"token"=> token,"rest"=>res}  
+    else
+    return {"success"=>false}    
+    end
+end
 
   
--- src/controllers/auth.cr
+++ src/controllers/auth.cr
@@ -4,7 +4,7 @@ require "json"
 class Auth < Application
     # ...
   
-    post "/login", :user do
+    post "/login", :log do
       # e.g. render a list of features detected on the photo
 
       json = JSON.build do |json|
@@ -24,6 +24,20 @@ class Auth < Application
     # POST /my_photos/:id/feature
     post "/register", :msg do
       # add a feature to the photo
-      head :ok
+ # e.g. render a list of features detected on the photo
+
+      json = JSON.build do |json|
+      json.object do
+        json.field "username", params["username"]
+        json.field "email" ,params["email"]
+        json.field "password" ,params["password"]
+      end
+      end
+       
+        response.headers["Access-Control-Allow-Origin"]="http://localhost:3000"
+        respond_with do 
+        render json: register(json)
+        end
+    #   auth(ReqData.from_json(request.body.not_nil!))
     end
   end
